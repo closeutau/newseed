8a35b462141123ac6e710d04fbd9afb80787446a
NOAH01112 <93462843+NOAH01112@users.noreply.github.com>
2024-05-08 00:38:35 +0900

    Add developer

--- routes/acl.js
+++ routes/acl.js
@@ -276,6 +276,7 @@ router.all(/^\/acl\/(.*)$/, async(req, res, next) => {
 						<option value=everyone${dbdata.read == 'everyone' ? ' selected' : ''}>모두</option>
 						<option value=member${dbdata.read == 'member' ? ' selected' : ''}>로그인한 사용자</option>
 						<option value=admin${dbdata.read == 'admin' ? ' selected' : ''}>관리자</option>
+						<option value=admin${dbdata.read == 'developer' ? ' selected' : ''}>개발자</option>
 					</select>
 				</div>
 				
@@ -285,6 +286,7 @@ router.all(/^\/acl\/(.*)$/, async(req, res, next) => {
 						<option value=everyone${dbdata.edit == 'everyone' ? ' selected' : ''}>모두</option>
 						<option value=member${dbdata.edit == 'member' ? ' selected' : ''}>로그인한 사용자</option>
 						<option value=admin${dbdata.edit == 'admin' ? ' selected' : ''}>관리자</option>
+						<option value=admin${dbdata.read == 'developer' ? ' selected' : ''}>개발자</option>
 					</select>
 				</div>
 				
@@ -294,6 +296,7 @@ router.all(/^\/acl\/(.*)$/, async(req, res, next) => {
 						<option value=everyone${dbdata.del == 'everyone' ? ' selected' : ''}>모두</option>
 						<option value=member${dbdata.del == 'member' ? ' selected' : ''}>로그인한 사용자</option>
 						<option value=admin${dbdata.del == 'admin' ? ' selected' : ''}>관리자</option>
+						<option value=admin${dbdata.read == 'developer' ? ' selected' : ''}>개발자</option>
 					</select>
 				</div>
 				
@@ -303,6 +306,7 @@ router.all(/^\/acl\/(.*)$/, async(req, res, next) => {
 						<option value=everyone${dbdata.discuss == 'everyone' ? ' selected' : ''}>모두</option>
 						<option value=member${dbdata.discuss == 'member' ? ' selected' : ''}>로그인한 사용자</option>
 						<option value=admin${dbdata.discuss == 'admin' ? ' selected' : ''}>관리자</option>
+						<option value=admin${dbdata.read == 'developer' ? ' selected' : ''}>개발자</option>
 					</select>
 				</div>
 				
@@ -312,6 +316,7 @@ router.all(/^\/acl\/(.*)$/, async(req, res, next) => {
 						<option value=everyone${dbdata.move == 'everyone' ? ' selected' : ''}>모두</option>
 						<option value=member${dbdata.move == 'member' ? ' selected' : ''}>로그인한 사용자</option>
 						<option value=admin${dbdata.move == 'admin' ? ' selected' : ''}>관리자</option>
+						<option value=admin${dbdata.read == 'developer' ? ' selected' : ''}>개발자</option>
 					</select>
 				</div>
 				
